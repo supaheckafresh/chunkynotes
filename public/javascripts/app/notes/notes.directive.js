cd868af5866b2c4293025b42e57846555f7ec56c
supaheckafresh <neilstrain@gmail.com>
2016-02-17 20:30:00 -0800

    Add notes list view to app

---
+++ public/javascripts/app/notes/notes.directive.js
@@ -0,0 +1,16 @@
+(function () {
+
+    'use strict';
+
+    angular.module('customDirectives')
+        .directive('notesList', function () {
+            return {
+                restrict: 'E',
+                scope: {
+                    info: '='
+                },
+                templateUrl: 'build/partials/app/notes/notes.html'
+            }
+        })
+
+}());
