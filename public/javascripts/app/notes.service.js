1352e15e6647b4d8f79deaa31aa51d31cb3e7223
supaheckafresh <neilstrain@gmail.com>
2016-02-17 21:02:15 -0800

    Add notes.service.js

--- public/javascripts/app/notes.service.js
+++ public/javascripts/app/notes.service.js
@@ -3,8 +3,27 @@
     'use strict';
 
     angular.module('app')
-        .service('NotesService', function () {
+        .service('NotesService', function (Note, $http) {
             var vm = this;
+
+            var notes = [];
+
+            vm.makeNotes = function (data) {
+                _.each(data, function (l) {
+                    vm.notes.push(new Note(l));
+                });
+                return vm.notes;
+            };
+
+            vm.getNotes = function () {
+                return $http.get('build/data/fakenotes.json')
+                    .then(function (res) {
+                        return vm.makeNotes(res.data);
+                    }, function (err) {
+                        console.log(err);
+                        return 'Sorry, there has been an error...';
+                    });
+            };
 
         });
 
