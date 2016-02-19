d27c701d60103981920adb3bb7eba479a579c204
supaheckafresh <neilstrain@gmail.com>
2016-02-18 17:29:21 -0800

    permanently save edited note and update date to now

--- public/javascripts/app/notes/note.controller.js
+++ public/javascripts/app/notes/note.controller.js
@@ -3,8 +3,13 @@
     'use strict';
 
     angular.module('app')
-        .controller('NoteController', function (note) {
+        .controller('NoteController', function (note, NotesService) {
             var vm = this;
             vm.note = note;
+
+            vm.update = function () {
+                vm.note.date = Date.now();
+                NotesService.saveToLocalStorage();
+            }
         });
 }());
